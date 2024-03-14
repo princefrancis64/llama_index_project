3092aa7d824a7e425a40443733ad97e56c3238c6
princefrancis64 <prince.francis64@gmail.com>
2024-03-14 07:59:07 +0530

    multilingual rag

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,70 @@
-## Important libraries
-
 import streamlit as st
-from llama_index.core import VectorStoreIndex,ServiceContext,Document,SimpleDirectoryReader
-# from llama_index.llms import OpenAI
 from openai import OpenAI
-import openai
-
-## Initialize message history
-openai.api_key = st.secrets.openai_key
-st.header("Chat  💬 📚")
-
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [
-        {"role":"assistant","content":"Ask me a question about the uploaded document!"}
-
-    ]
+import os
+from llama_index.core import (VectorStoreIndex,SimpleDirectoryReader,StorageContext,load_index_from_storage)
 
 
-## Load and index data
-@st.cache_resource(show_spinner=False)
-def load_data():
-    with st.spinner(text="Loading and indexing the doc – hang tight! This should take 1-2 minutes."):
-        document = SimpleDirectoryReader(input_dir ="./data",recursive=True).load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo",temperature=0.2,system_prompt="You are an expert on the YOLO, the popular object detection algorithm that has revolutionized the field of computer vision and your job is to answer technical questions. Assume that all questions are related to YOLO only. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_documents(document,service_context=service_context)
-        return index
+#### Converting the audio/video file into a text format
+client = OpenAI()
+audio_file_path = "./audio_files/yolo_dutch.mp3"
+audio_file = open(audio_file_path,"rb")
+translation = client.audio.translations.create(
+    model="whisper-1",
+    file=audio_file
+)
 
-index=load_data()
+### storing the translated text into the folder ./data
+file_storage_path = os.path.join("./data/data.txt")
+with open(file=file_storage_path,mode="w") as file:
+    file.write(translation.text)
 
-## Creating a chat engine
-chat_engine = index.as_chat_engine(chat_mode="condense_question",verbose=True)
+### checking if storage already exists
+PERSIST_DIR ="./storage"
+if not os.path.exists(PERSIST_DIR):
+    ## load the documents and create the index
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    ## store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    ## load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index=load_index_from_storage(storage_context)
+
+#### QUERYING THE DATA
+query_engine=index.as_query_engine(similarity_top_k=5,streaming=True)
+
+## getting a function to pass the prompt 
+def response(prompt):
+    return query_engine.query(prompt)
 
 
-## Prompt for user input and display message history
-if prompt :=st.chat_input("Your Question"):
-    st.session_state.messages.append({"role":"user","content":prompt})
+st.title("Multilingual RAG")
 
+## Set OpenAI API key from streamlit secrets
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+
+## Initialize the chat history
+if "messages" not in st.session_state:
+    st.session_state.messages=[]
+
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.write(message["content"])
+        st.markdown(message["content"])
 
+# Accept user input
+if prompt:=st.chat_input("Your question...."):
+    ## Adding user message to chat history
+    st.session_state.messages.append({"role":"user","content":prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-## Pass query to chat engine and display response
-# If last message is not from assistant, generate a new response
-if st.session_state.message[-1]["role"]!="assistant":
+    # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        with st.spinner("Thinking...."):
-            response = chat_engine.chat(prompt)
-            st.write(response.response)
-            message={"role":"assistant","content":response.response}
-            st.session_state.messages.append(message)
+        response_ass=response(prompt=prompt)
+        st.markdown(response_ass)
+    ### Appending the response to the chat history
+    st.session_state.messages.append({"role":"assistant","content":response_ass})
